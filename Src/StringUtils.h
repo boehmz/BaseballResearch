2123aea69ab574284ff63612e1413fb001ea1c33
Zachary Boehm <zacharyboehm@yahoo.com>
2018-07-05 11:25:49 -0700

    ReplaceInString/EraseInString Added

--- Src/StringUtils.h
+++ Src/StringUtils.h
@@ -29,9 +29,10 @@ void RemoveAllSectionsWithKeyword(std::string& wholeString, const std::string& k
 std::string GetSubStringBetweenStrings(const std::string& wholeString, const std::string& leftString, const std::string& rightString);
 void CutStringToOnlySectionBetweenKeywords(std::string& wholeString, const std::string& sectionBegin, const std::string& sectionEnd);
 std::string ExtractStringToBeOnlySectionBetweenKeywords(const std::string& wholeString, const std::string& sectionBegin, const std::string& sectionEnd);
-
+void ReplaceInStringIfExists(std::string& originalString, const std::string& removeString, const std::string& replaceString);
+void EraseInString(std::string& originalString, const std::string& eraseString);
 std::vector<std::string> SplitStringIntoMultiple(std::string wholeString, std::string tokens, std::string removeFromIndividual = "");
-
+size_t FindPlayerNameIndexInList(const std::string& playerName, const std::string& searchText);
 int GetNumDaysInMonth(int monthInteger);
 
 void CurlGetSiteContents(CURL* curl, std::string readURL, std::string& writeBuffer, bool allowRedirects = false);
