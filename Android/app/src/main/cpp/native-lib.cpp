ed5a6af3bb4c20598179d621d3b57d8fc64e443f
boehmz <Zachary.Boehm@viacommix.com>
2019-01-10 18:47:28 -0800

    Basic libcurl android stuff working

--- Android/app/src/main/cpp/native-lib.cpp
+++ Android/app/src/main/cpp/native-lib.cpp
@@ -1,11 +1,59 @@
 #include <jni.h>
 #include <string>
+#include <curl/curl.h>
+#include <android/log.h>
+
+#define TRUE 1
+#define FALSE 0
+
+
+#ifdef ANDROID
+	#define LOGI(...) ((void)__android_log_print(ANDROID_LOG_INFO, "MLBPredictor", __VA_ARGS__))
+#else
+	#define LOGI(...) printf(__VA_ARGS__)
+#endif
+
+size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp)
+{
+    if (userp != NULL)
+        ((std::string*)userp)->append((char*)contents, size * nmemb);
+    return size * nmemb;
+}
+
+std::string GetSiteHtml() {
+
+    CURL *curl = curl_easy_init();
+    if (curl)
+    {
+
+        std::string httpBuffer;
+        std::string thisPositionURL = "http://rotoguru1.com/cgi-bin/stats.cgi?pos=1&sort=4";
+        thisPositionURL = "https://www.fangraphs.com/dailyprojections.aspx?pos=all&stats=bat&type=sabersim&team=0&lg=all&players=0";
+        curl_easy_setopt(curl, CURLOPT_URL, thisPositionURL.c_str());
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &httpBuffer);
+        curl_easy_setopt(curl, CURLOPT_FAILONERROR, TRUE);
+        curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, TRUE);
+        curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, FALSE);
+	    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, FALSE);
+
+        CURLcode res = curl_easy_perform(curl);
+        if (res != CURLE_OK){
+            LOGI("CURL failed with error code %d", res);
+        }
+        curl_easy_reset(curl);
+        int testInt = 0;
+        LOGI("Html result from site %s was: %s", thisPositionURL.c_str(), httpBuffer.c_str());
+        return httpBuffer;
+
+    }
+    return "could not make curl";
+}
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_predictor_mlb_mlbpredictor_MainActivity_stringFromJNI(
         JNIEnv* env,
         jobject /* this */) {
-    std::string hello = "Hello from C++";
-
-    return env->NewStringUTF(hello.c_str());
+    std::string readBuffer = GetSiteHtml();
+    return env->NewStringUTF(readBuffer.substr(0,100).c_str());
 }
---
+++ Android/app/src/main/jni/Android.mk
@@ -0,0 +1,30 @@
+LOCAL_PATH := $(call my-dir)
+
+#cURL prebuilt
+include $(CLEAR_VARS)
+LOCAL_MODULE := curl-prebuilt
+LOCAL_SRC_FILES := \
+  ../libcurl_android_prebuilt/$(TARGET_ARCH_ABI)/libcurl.a
+include $(PREBUILT_STATIC_LIBRARY)
+################################################################################
+
+#Test Library
+include $(CLEAR_VARS)
+LOCAL_MODULE := native-lib
+LOCAL_SRC_FILES := ../cpp/native-lib.cpp
+LOCAL_STATIC_LIBRARIES := curl-prebuilt 
+COMMON_CFLAGS := -Werror -DANDROID 
+
+ifeq ($(TARGET_ARCH),arm)
+  LOCAL_CFLAGS := -mfpu=vfp -mfloat-abi=softfp -fno-short-enums
+endif
+
+LOCAL_CFLAGS += $(COMMON_CFLAGS)
+LOCAL_LDLIBS := -lz -llog -Wl,-s
+LOCAL_CPPFLAGS += -std=gnu++0x
+LOCAL_C_INCLUDES += \
+  $(NDK_PATH)/platforms/$(TARGET_PLATFORM)/arch-$(TARGET_ARCH)/usr/include \
+  $(LOCAL_PATH)/../libcurl_android_prebuilt/include
+
+include $(BUILD_SHARED_LIBRARY)
+################################################################################
